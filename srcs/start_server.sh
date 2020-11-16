177768f0e9ddba47becd1110bef657d5b3421283
Daniel Rodriguez pueyo <danrodri@c1r10s3.42madrid.com>
2020-11-16 21:17:12 +0100

    small fixes

--- srcs/start_server.sh
+++ srcs/start_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ "$AIENV" == "off" ]; then
-	sed -i /etc/nginx/sites-available/default -e 's/autoindex on/autoindex off/'
+	sed -i /etc/nginx/sites-available/localhost.conf -e 's/autoindex on/autoindex off/'
 fi
 service php7.3-fpm start
 service mysql start
